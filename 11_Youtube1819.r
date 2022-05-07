95753a9ea33a62f1d9d292823dbea59e751cb771
Hot6x <95900411+Hot6x@users.noreply.github.com>
2022-05-07 14:53:31 +0900

    2022.05.07.12

--- 11_Youtube1819.r
+++ 11_Youtube1819.r
@@ -5,18 +5,15 @@ library(stringr)
 library(wordcloud)
 useNIADic()
 
-
 # 사전 추가
-mergeUserDic(data.frame(readLines("ytube_dic.txt"), "ncn"))
-
+mergeUserDic(data.frame(readLines("DIC.txt"), "ncn"))
 
 # 형태소 분석
-ytube <- readLines("youtube_b.txt", encoding = "UTF-8")
+ytube <- readLines("youtube_1819.txt", encoding = "UTF-8")
 head(ytube, 5)
 ytube_2 <- unique(ytube)
 ytube_ext <- extractNoun(ytube_2)
 ytube_ext_2 <- lapply(ytube_ext, unique)
-
 
 # 불용어 제거
 cdata <- unlist(ytube_ext_2)
@@ -28,6 +25,7 @@ head(sort(wordcount, decreasing = T), 10)
 ytube_un <- gsub("VLOG", "브이로그", ytube_un)
 ytube_un <- gsub("vlog", "브이로그", ytube_un)
 ytube_un <- gsub("Vlog", "브이로그", ytube_un)
+ytube_un <- gsub("들이", "나들이", ytube_un)
 
 # 불용어 사전
 txt_gsub <- readLines("ytube_gsub.txt", encoding = "UTF-8")
@@ -36,26 +34,19 @@ for (i in 1:cnt_gsub) {
     ytube_un <- gsub((txt_gsub[i]), "", ytube_un)
 }
 
-
 # 글자수로 제거
 ytube_un <- Filter(function(x) {
     nchar(x) >= 2 & nchar(x) <= 15
 }, ytube_un)
 
-
-# 확인
 wordcount <- table(ytube_un)
 head(sort(wordcount, decreasing = T), 100)
 
-
-###
-
-wordcount2 <- head(sort(wordcount, decreasing = T), 300)
+wordcount2 <- head(sort(wordcount, decreasing = T), 100)
 palete <- brewer.pal(7, "Set1")
 wordcloud(names(wordcount2),
     freq = wordcount2, scale = c(5, 1), rot.per = 0.25, min.freq = 4,
     random.order = F, random.color = T, colors = palete
 )
 
-
-#
+write.table(wordcount2, "WC_youtube1819.txt", fileEncoding = "UTF-8")
