e56745d5335e8b452f57f8e066a37bc294cf472d
Hot6x <95900411+OrcunLee@users.noreply.github.com>
2022-06-16 10:33:44 +0900

    2022.06.15.24

--- hunet_FP/Crawler/incruit_head.py
+++ hunet_FP/Crawler/incruit_head.py
@@ -1,4 +1,5 @@
 # %%
+from pickle import TRUE
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -6,9 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import time, math, os, random, urllib, urllib.request, getpass, re, datetime
+import time, math, os, random, urllib, urllib.request, getpass, re, datetime, tqdm
 
-# %%
 Services = Service("C:/Users/yzz07/Desktop/PROGRAMMING/chromedriver.exe")
 driver = webdriver.Chrome(service=Services)
 today = datetime.datetime.today()
@@ -56,88 +56,131 @@ for key, value in dic_category.items():
                 data_middle_ = i.find('div', 'cell_mid').find('div', 'cl_btm').text
                 data_main.append(data_main_)
                 data_middle.append(data_middle_)
-
             page += 1
     except :
         print('%s' %value)
 
-
-# %%
 df = pd.DataFrame()
 df['data_url'] = pd.Series(data_url)
 df['data_main'] = pd.Series(data_main)
 df['data_middle'] = pd.Series(data_middle)
 
-# %%
 fc_name = (time_name + name + '.csv')
 df.to_csv(fc_name, index=False, encoding="utf-8-sig")
 
 # %%
-data_type = []  # 고용형태
-data_career = []  # 경력
-data_education = []  # 학력
-data_region = []  # 지역
-data_title = []  # 제목
-data_company = []  # 기업 이름
-data_deadline = []  # 마감일
-data_notice = []  # 공고일
-data_size = []
-data_field = []
+nums = 0
 
-for i in data_url :
-    url = i
-    Services = Service("C:/Users/yzz07/Desktop/PROGRAMMING/chromedriver.exe")
-    driver = webdriver.Chrome(service=Services)
-    driver.get(url)
-    time.sleep(random.uniform(1, 3)) 
+while True:
+    try:
+        df = pd.read_csv('%surl.csv' % nums)
+        data_url = df['data_url']
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    content = soup.find('div', 'section_view_layout')
+    except:
+        data_url = df['data_url']
 
-    data_type_ = content.find_all('em', class_="bb")[-3].get_text()
-    data_career_ = content.find_all('em', class_="bb")[-2].get_text()
-    data_education_ = content.find_all('em', class_="bb")[-1].get_text()
-    data_region_ = content.find_all('em')[-5].get_text()
-    data_title_ = content.find('div', 'job_info_detail').find('h2').find("strong").get_text()
-    data_company_ = content.find('div', 'job_info_detail').find('h3').find("a").get_text()
+    print('Number of work to be done : ', len(data_url))
+    if len(data_url) == 0:
+        break
 
-    try :
-        data_deadline_ = soup.find_all('div', 'day')[1].find('em').text
-    except :
-        data_deadline_ = '채용시'
+    data_type = []  # 고용형태
+    data_career = []  # 경력
+    data_education = []  # 학력
+    data_region = []  # 지역
+    data_title = []  # 제목
+    data_company = []  # 기업 이름
+    data_deadline = []  # 마감일
+    data_notice = []  # 공고일
+    data_size = []
+    data_field = []
 
-    try :
-        data_notice_ = soup.find_all('div', 'day')[0].text
-    except :
-        data_notice_ = '-'
+    for i in range(0, len(data_url)):
+        url = data_url[i]
+        try:
+            Services = Service(
+                "C:/Users/yzz07/Desktop/PROGRAMMING/chromedriver.exe")
+            driver = webdriver.Chrome(service=Services)
+            driver.get(url)
+            time.sleep(random.uniform(2, 4))
+            html = driver.page_source
+            soup = BeautifulSoup(html, "html.parser")
+            content = soup.find('div', 'section_view_layout')
 
-    data_size_ = content.find('div', 'job_info_detail').find_all('dd')[-1].get_text()
-    data_field_ = content.find('div', 'job_info_detail').find_all('dd')[-2].get_text()
+        except:
+            print('URL where the error occurred : ', url)
+            print('Number of works : ', i)
+            print('-' * 50)
 
-    data_type.append(data_type_)
-    data_career.append(data_career_)
-    data_education.append(data_education_)
-    data_region.append(data_region_)
-    data_title.append(data_title_)
-    data_company.append(data_company_)
-    data_deadline.append(data_deadline_)
-    data_notice.append(data_notice_)
-    data_size.append(data_size_)
-    driver.close()
+            if i == 0:
+                break
 
+            else:
+                ef = pd.DataFrame()
+                ef['data_url'] = data_url[i:]
+                nums += 1
+                ef_name = ('%surl.csv' % nums)
+                ef.to_csv(ef_name, index=False, encoding="utf-8-sig")
+                break
 
-# %%
-df['data_type'] = pd.Series(data_type)
-df['data_career'] = pd.Series(data_career)
-df['data_education'] = pd.Series(data_education)
-df['data_region'] = pd.Series(data_region)
-df['data_title'] = pd.Series(data_title)
-df['data_company'] = pd.Series(data_company)
-df['data_deadline'] = pd.Series(data_deadline)
-df['data_notice'] = pd.Series(data_notice)
-df['data_size'] = pd.Series(data_size)
-# %%
-fc_name = (time_name + name + '2.csv')
-df.to_csv(fc_name, index=False, encoding="utf-8-sig")
+        try:
+            data_type_ = content.find_all('em', class_="bb")[-3].get_text()
+            data_career_ = content.find_all('em', class_="bb")[-2].get_text()
+            data_education_ = content.find_all(
+                'em', class_="bb")[-1].get_text()
+            data_region_ = content.find_all('em')[-5].get_text()
+            data_title_ = content.find('div', 'job_info_detail').find(
+                'h2').find("strong").get_text()
+            data_company_ = content.find('div', 'job_info_detail').find(
+                'h3').find("a").get_text()
+
+            try:
+                data_deadline_ = soup.find_all('div', 'day')[1].find('em').text
+            except:
+                data_deadline_ = '채용시'
+
+            try:
+                data_notice_ = soup.find_all('div', 'day')[0].text
+            except:
+                data_notice_ = '-'
+
+            data_size_ = content.find(
+                'div', 'job_info_detail').find_all('dd')[-1].get_text()
+            data_field_ = content.find(
+                'div', 'job_info_detail').find_all('dd')[-2].get_text()
+
+        except:
+            data_type_ = '-'
+            data_career_ = '-'
+            data_education_ = '-'
+            data_region_ = '-'
+            data_title_ = '-'
+            data_company_ = '-'
+            data_deadline_ = '-'
+            data_notice_ = '-'
+
+        finally:
+            data_type.append(data_type_)
+            data_career.append(data_career_)
+            data_education.append(data_education_)
+            data_region.append(data_region_)
+            data_title.append(data_title_)
+            data_company.append(data_company_)
+            data_deadline.append(data_deadline_)
+            data_notice.append(data_notice_)
+            data_size.append(data_size_)
+            time.sleep(random.uniform(0.1, 2))
+
+    df['data_type'] = pd.Series(data_type)
+    df['data_career'] = pd.Series(data_career)
+    df['data_education'] = pd.Series(data_education)
+    df['data_region'] = pd.Series(data_region)
+    df['data_title'] = pd.Series(data_title)
+    df['data_company'] = pd.Series(data_company)
+    df['data_deadline'] = pd.Series(data_deadline)
+    df['data_notice'] = pd.Series(data_notice)
+    df['data_size'] = pd.Series(data_size)
+
+    fc_name = (time_name + name + '%s.csv' % nums)
+    df.to_csv(fc_name, index=False, encoding="utf-8-sig")
+
 # %%
